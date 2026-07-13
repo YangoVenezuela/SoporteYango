7d5f12983e3b21f1cf8f84aecc5d2b26dfce0d0c
YangoVenezuela <yangovenezuela@gmail.com>
2026-07-13 13:01:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api'); 
 const express = require('express');
 const fetch = require('node-fetch');
 
